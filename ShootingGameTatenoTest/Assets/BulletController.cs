a6ec3fbad00fba265fd1c35163dc3207e8038e2a
lucatrab1905 <luca.trabuio@gmail.com>
2025-12-05 12:00:10 +0900

    MorningWork

---
+++ ShootingGameTatenoTest/Assets/BulletController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Translate(0, 0.1f, 0);
+        if (transform.position.y > 5)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
